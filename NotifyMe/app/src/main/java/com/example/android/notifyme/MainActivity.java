c797bc0b94899cdff104f64206bbbd8ab5dd780d
Logan Thomas <ltogan.2014@gmail.com>
2019-05-23 10:59:07 -0400

    Add a content intent and dismiss the notification in Notify Me! project

--- NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.notifyme;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+
+        createNotificationChannel();
     }
 
     public void sendNotification() {
@@ -54,9 +58,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private NotificationCompat.Builder getNotificationBuilder() {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent notificationPendingIntent = PendingIntent.getActivity(this,
+                NOTIFICATION_ID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         return new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
                 .setContentTitle("You've been notified!")
                 .setContentText("This is your notification text.")
-                .setSmallIcon(R.drawable.ic_android);
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentIntent(notificationPendingIntent)
+                .setAutoCancel(true);
     }
 }
