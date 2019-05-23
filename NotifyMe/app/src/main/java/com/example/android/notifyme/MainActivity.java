06cf9feb30cc7000593e8fd135aafc3a69e356c5
Logan Thomas <ltogan.2014@gmail.com>
2019-05-23 10:52:58 -0400

    Build a notification in Notify Me! project

--- NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
@@ -4,14 +4,17 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NOTIFICATION_ID = 0;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private NotificationManager mNotifyManager;
+
     private Button button_notify;
 
     @Override
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotifyManager.notify(NOTIFICATION_ID, notifyBuilder.build());
     }
 
     public void createNotificationChannel() {
@@ -46,5 +51,12 @@ public class MainActivity extends AppCompatActivity {
             notificationChannel.setDescription("Notification from Mascot");
             mNotifyManager.createNotificationChannel(notificationChannel);
         }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        return new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("You've been notified!")
+                .setContentText("This is your notification text.")
+                .setSmallIcon(R.drawable.ic_android);
     }
 }
