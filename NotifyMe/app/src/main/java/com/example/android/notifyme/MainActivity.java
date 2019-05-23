46a4baa82430f4a9006e03945dea18d9170a5323
Logan Thomas <ltogan.2014@gmail.com>
2019-05-23 10:45:14 -0400

    Update layout of Notify Me! project

--- NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/example/android/notifyme/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.notifyme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_notify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_notify = findViewById(R.id.notify);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendNotification();
+            }
+        });
+    }
+
+    public void sendNotification() {
     }
 }
