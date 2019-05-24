efbe75e30bb6c8ee55f52524a9c22c28e7e6b3b5
Logan Thomas <ltogan.2014@gmail.com>
2019-05-24 16:55:48 -0400

    Restore preferences in onCreate in HelloSharedPrefs project

--- HelloSharedPrefs/app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
+++ HelloSharedPrefs/app/src/main/java/com/example/android/hellosharedprefs/MainActivity.java
@@ -61,17 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
         mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
 
-        // Restore the saved instance state.
-        if (savedInstanceState != null) {
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mShowCountTextView.setText(String.format("%s", mCount));
 
-            mCount = savedInstanceState.getInt(COUNT_KEY);
-            if (mCount != 0) {
-                mShowCountTextView.setText(String.format("%s", mCount));
-            }
-
-            mColor = savedInstanceState.getInt(COLOR_KEY);
-            mShowCountTextView.setBackgroundColor(mColor);
-        }
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
     }
 
     @Override
